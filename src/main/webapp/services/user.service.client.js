dda839f914e2515673bcb20831f64e5946110138
Samantha Becker <Sam@MacBook-Pro.local>
2021-02-09 19:37:42 -0500

    services completed

--- src/main/webapp/services/user.service.client.js
+++ src/main/webapp/services/user.service.client.js
@@ -4,11 +4,46 @@ function AdminUserServiceClient() {
     this.findUserById = findUserById;
     this.deleteUser = deleteUser;
     this.updateUser = updateUser;
-    this.url = 'https://wbdv-generic-server.herokuapp.com/api/YOURNEUID/users';
+    this.url = 'https://wbdv-generic-server.herokuapp.com/api/001063084/users';
     var self = this;
-    function createUser(user) { … }
-    function findAllUsers() { … }
-    function findUserById(userId) { … }
-    function updateUser(userId, user) { … }
-    function deleteUser(userId) { … }
+
+    function createUser(user) {
+        return fetch(self.url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        }).then(function (response) {
+            return response.json()
+            })
+    }
+
+    function findAllUsers() {
+        return fetch(self.url).then((response) =>
+            response.json());
+    }
+
+    function findUserById(userId) {
+        return fetch(`${self.url}/${userId}`).then(response =>
+            response.json());
+    }
+
+    function updateUser(userId, user) {
+        return fetch(`${self.url}/${userId}`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        }).then(response => response.json());
+    }
+
+    function deleteUser(userId) {
+        return fetch(`${self.url}/${userId}`, {
+            method: 'DELETE'
+        }).then(function (response) {
+            return response.json()
+            })
+    }
 }
