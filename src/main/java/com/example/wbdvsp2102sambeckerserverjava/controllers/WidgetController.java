40a6daa2839b5d4dac1dcf51ae521f712c602eb7
Samantha Becker <Sam@MacBook-Pro.local>
2021-03-17 23:07:13 -0400

    WidgetController + WidgetService editted. tid type changed from int to String

---
+++ src/main/java/com/example/wbdvsp2102sambeckerserverjava/controllers/WidgetController.java
@@ -0,0 +1,53 @@
+package com.example.wbdvsp2102sambeckerserverjava.controllers;
+
+import com.example.wbdvsp2102sambeckerserverjava.models.Widget;
+import com.example.wbdvsp2102sambeckerserverjava.services.WidgetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class WidgetController {
+  @Autowired
+  WidgetService service;// = new WidgetService();
+
+  @PostMapping("/api/topics/{tid}/widgets")
+  public Widget createWidget(
+          @PathVariable("tid") String topicId,
+          @RequestBody Widget widget) {
+    widget.setTopicId(topicId);
+    return service.createWidget(topicId, widget);
+  }
+
+  @GetMapping("/api/topics/{tid}/widgets")
+  public List<Widget> findWidgetsForTopic(
+          @PathVariable("tid") String topicId
+  ) {
+    return service.findWidgetsForTopic(topicId);
+  }
+
+  @GetMapping("/api/widgets")
+  public List<Widget> findAllWidgets() {
+    return service.findAllWidgets();
+  }
+
+  @GetMapping("/api/widgets/{wid}")
+  public Widget findWidgetById(
+          @PathVariable("wid") String id) {
+    return service.findWidgetById(id);
+  }
+
+  @DeleteMapping("/api/widgets/{wid}")
+  public Integer deleteWidget(@PathVariable("wid") String id) {
+    return service.deleteWidget(id);
+  }
+
+  @PutMapping("/api/widgets/{wid}")
+  public Integer updateWidget(
+          @PathVariable("wid") String id,
+          @RequestBody Widget widget) {
+    return service.updateWidget(id, widget);
+  }
+}
---
+++ src/main/java/com/example/wbdvsp2102sambeckerserverjava/services/WidgetService.java
@@ -0,0 +1,70 @@
+package com.example.wbdvsp2102sambeckerserverjava.services;
+
+import com.example.wbdvsp2102sambeckerserverjava.models.Widget;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class WidgetService {
+  private final List<Widget> widgets = new ArrayList<Widget>();
+
+  // implement crud operations
+  // tid changed to String per spec
+  public Widget createWidget(String tid, Widget widget) {
+    Integer id = Math.toIntExact((new Date()).getTime());
+    widget.setId(id);
+    widgets.add(widget);
+    return widget;
+  }
+
+  public List<Widget> findWidgetsForTopic(String topicId) {
+    List<Widget> ws = new ArrayList<Widget>();
+    for(Widget w: widgets) {
+      if(w.getTopicId().equals(topicId)) {
+        ws.add(w);
+      }
+    }
+    return ws;
+  }
+
+  public Integer updateWidget(String id, Widget newWidget) {
+    for(int i=0; i<widgets.size(); i++) {
+      Widget w = widgets.get(i);
+      if(w.getId().equals(Integer.parseInt(id))) {
+        widgets.set(i, newWidget);
+        return 1;
+      }
+    }
+    return -1;
+  }
+  public Integer deleteWidget(String id) {
+    int index = -1;
+    for(int i=0; i<widgets.size(); i++) {
+      Widget w = widgets.get(i);
+      if(w.getId().equals(Integer.parseInt(id))) {
+        index = i;
+      }
+    }
+    if(index >= 0) {
+      widgets.remove(index);
+      return 1;
+    }
+    return -1;
+  }
+
+  public List<Widget> findAllWidgets() {
+    return widgets;
+  }
+
+  public Widget findWidgetById(String id) {
+    for(Widget w: widgets) {
+      if(w.getId().equals(Integer.parseInt(id))) {
+        return w;
+      }
+    }
+    return null;
+  }
+}
